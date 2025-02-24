d209d2d6c89643a7be517608eb65920261e31e57
AldairCruz501 <acruz.pbxhosting@gmail.com>
2025-02-24 12:31:31 -0600

    Actualizacion del sitio web tuis

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { config } from 'dotenv';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { config } from "dotenv";
 
 config();
 
@@ -10,26 +10,36 @@ export default defineConfig({
   server: {
     proxy: {
       "/imeis": {
-        target: "https://addinteli-prod-api.com.mx/imeis", // URL de la API
-        changeOrigin: true, // Cambiar el origen de la solicitud
-        secure: false, // Si la API es HTTPS, usa true
-        rewrite: (path) => path.replace(/^\/imeis/, ""), // Eliminar '/imeis' de la URL antes de enviarla
+        target: "https://tuis.cero208.mx/imeis",
+        //target: "http://localhost:3360/imeis", // URL de la API
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/imeis/, ""),
       },
       "/login": {
-        target: "https://api.cero208.mx/Auth/login", // URL de tu servidor local
+        target: "https://tuis.cero208.mx/Auth/login",
+        //target: "http://localhost:3360/Auth/login",
         changeOrigin: true,
-        secure: false, // Si el servidor es HTTPS, establece en true
-        rewrite: (path) => path.replace(/^\/login/, ""), // Ajusta según el nuevo endpoint
+        secure: false,
+        rewrite: (path) => path.replace(/^\/login/, ""),
       },
       "/createAccount": {
-        target: "https://api.cero208.mx/Auth/create", // URL de tu servidor local
+        target: "https://tuis.cero208.mx/Auth/create",
+        //target: "http://localhost:3360/Auth/create",
         changeOrigin: true,
-        secure: false, // Si el servidor es HTTPS, establece en true
-        rewrite: (path) => path.replace(/^\/createAccount/, ""), // Ajusta según el nuevo endpoint
+        secure: false,
+        rewrite: (path) => path.replace(/^\/createAccount/, ""),
+      },
+      "/create-checkout-session": {
+        target: "https://tuis.cero208.mx/payment/create-checkout-session",
+        // target: "http://localhost:3360/payment/create-checkout-session",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/create-checkout-session/, ""),
       },
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
 });
